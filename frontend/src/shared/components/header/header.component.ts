2293878ba6bec45b813f58ac7b7042fda8b44e66
Guilherme Bastos Leone <guilhermebastosleone@gmail.com>
2026-01-14 20:14:17 -0300

    (feat); search bar.

--- frontend/src/shared/components/header/header.component.ts
+++ frontend/src/shared/components/header/header.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 import { SearchBarComponent } from "../search-bar/search-bar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage, MatIconModule, MatButtonModule, SearchBarComponent],
+  imports: [NgOptimizedImage, MatIconModule, MatButtonModule, SearchBarComponent, MatTooltipModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- frontend/src/shared/components/search-bar/search-bar.component.ts
+++ frontend/src/shared/components/search-bar/search-bar.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  imports: [MatTooltipModule, MatButtonModule, MatIconModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
 export class SearchBarComponent {
+  isClicked = signal(false);
 
+  toggleSearchBar() {
+    this.isClicked.set(!this.isClicked())
+  }
 }
